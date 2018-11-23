a74c6f338b5df1521bd88597cec4d9194adc5a91
Philip Stanislaus <stanislaus.philip@gmail.com>
2018-11-23 12:53:16 +0100

    Add further failure logs

--- collect.sh
+++ collect.sh
@@ -5,42 +5,53 @@ fi
 
 mkdir -p $1/status
 
-# echo "Loading status of node0"
-# curl -f http://18.223.204.76:26657/status > $1/status/node0.txt
+echo "Loading status of node0"
+curl -f http://18.217.7.174:26657/status > $1/status/node0.txt
 echo "Loading status of node1"
-curl -f http://52.15.174.195:26657/status > $1/status/node1.txt
+curl -f http://18.188.83.104:26657/status > $1/status/node1.txt
 echo "Loading status of node2"
-curl -f http://18.222.75.1:26657/status > $1/status/node2.txt
+curl -f http://18.224.214.251:26657/status > $1/status/node2.txt
 echo "Loading status of node3"
-curl -f http://18.223.177.85:26657/status > $1/status/node3.txt
+curl -f http://18.188.224.197:26657/status > $1/status/node3.txt
 
 mkdir -p $1/dump_consensus_state
 
-# echo "Loading dump_consensus_state of node0"
-# curl -f http://18.223.204.76:26657/dump_consensus_state > $1/dump_consensus_state/node0.txt
+echo "Loading dump_consensus_state of node0"
+curl -f http://18.217.7.174:26657/dump_consensus_state > $1/dump_consensus_state/node0.txt
 echo "Loading dump_consensus_state of node1"
-curl -f http://52.15.174.195:26657/dump_consensus_state > $1/dump_consensus_state/node1.txt
-# echo "Loading dump_consensus_state of node2"
-# curl -f http://18.222.75.1:26657/dump_consensus_state > $1/dump_consensus_state/node2.txt
+curl -f http://18.188.83.104:26657/dump_consensus_state > $1/dump_consensus_state/node1.txt
+echo "Loading dump_consensus_state of node2"
+curl -f http://18.224.214.251:26657/dump_consensus_state > $1/dump_consensus_state/node2.txt
 echo "Loading dump_consensus_state of node3"
-curl -f http://18.223.177.85:26657/dump_consensus_state > $1/dump_consensus_state/node3.txt
+curl -f http://18.188.224.197:26657/dump_consensus_state > $1/dump_consensus_state/node3.txt
 
 mkdir -p $1/logs
 
 echo "Extracting logs of node0"
-ssh ec2-user@18.223.204.76 "journalctl > log.txt"
+ssh centos@18.217.7.174 "journalctl > log.txt"
 echo "Extracting logs of node1"
-ssh ec2-user@52.15.174.195 "journalctl > log.txt"
+ssh centos@18.188.83.104 "journalctl > log.txt"
 echo "Extracting logs of node2"
-ssh ec2-user@18.222.75.1 "journalctl > log.txt"
+ssh centos@18.224.214.251 "journalctl > log.txt"
 echo "Extracting logs of node3"
-ssh ec2-user@18.223.177.85 "journalctl > log.txt"
+ssh centos@18.188.224.197 "journalctl > log.txt"
 
 echo "Loading logs of node0"
-scp ec2-user@18.223.204.76:~/log.txt $1/logs/log-node0.txt
+scp centos@18.217.7.174:~/log.txt $1/logs/log-node0.txt
 echo "Loading logs of node1"
-scp ec2-user@52.15.174.195:~/log.txt $1/logs/log-node1.txt
+scp centos@18.188.83.104:~/log.txt $1/logs/log-node1.txt
 echo "Loading logs of node2"
-scp ec2-user@18.222.75.1:~/log.txt $1/logs/log-node2.txt
+scp centos@18.224.214.251:~/log.txt $1/logs/log-node2.txt
 echo "Loading logs of node3"
-scp ec2-user@18.223.177.85:~/log.txt $1/logs/log-node3.txt
+scp centos@18.188.224.197:~/log.txt $1/logs/log-node3.txt
+
+mkdir -p $1/df
+
+echo "Getting df of node0"
+ssh centos@18.217.7.174 "df -h" > $1/df/node0.txt
+echo "Getting df of node1"
+ssh centos@18.188.83.104 "df -h" > $1/df/node1.txt
+echo "Getting df of node2"
+ssh centos@18.224.214.251 "df -h" > $1/df/node2.txt
+echo "Getting df of node3"
+ssh centos@18.188.224.197 "df -h" > $1/df/node3.txt
